0f8280e1ccd8c20a532a22bb8463a8ab2b51d409
Stefano Meschiari <smeschiari@duosecurity.com>
2018-09-22 17:32:32 -0500

    Completed version

--- notebooks/init.py
+++ notebooks/init.py
@@ -3,6 +3,8 @@ import pyspark
 import pyspark.sql.functions as F
 import seaborn as sns
 import pandas as pd
+import numpy as np
+import matplotlib.ticker as ticker
 
 # Show a bit more of content
 pd.set_option('display.max_colwidth', 100)
@@ -22,8 +24,11 @@ def convert_to_pandas(df, n):
 
     return df.limit(n).toPandas()
 
+@ticker.FuncFormatter
+def log_formatter(x, pos):
+    return "$10^{%s}$" % x
 
-def plot(self, x=None, y=None, kind="scatter", hue=None, n=100000, logx=False, logy=False):
+def plot(self, x=None, y=None, kind="scatter", hue=None, n=100000, logx=False, logy=False, norm_hist=False):
     df = convert_to_pandas(self.cache(), n)
 
     if kind == "scatter":
@@ -31,14 +36,22 @@ def plot(self, x=None, y=None, kind="scatter", hue=None, n=100000, logx=False, l
     elif kind == "line":
         grid = sns.lineplot(x=x, y=y, hue=hue, data=df)
     elif kind == "hist":
-        grid = sns.distplot(df[x], kde=False)
+        series = df[x]
+        if logx:
+            series = np.log10(series[series > 0.0])
+        
+        grid = sns.distplot(series, kde=False,  norm_hist=norm_hist)
+        if logx:
+            grid.xaxis.set_major_formatter(log_formatter)
     else:
         raise ValueError("kind should be one of: scatter, line, hist")
 
-    if logx:
+    if logx and not kind == "hist":
         grid.set(xscale="log")
     if logy:
         grid.set(yscale="log")
+
+    return grid
 
 # Horrible, horrible monkey patching
 pyspark.sql.dataframe.DataFrame.plot = plot
